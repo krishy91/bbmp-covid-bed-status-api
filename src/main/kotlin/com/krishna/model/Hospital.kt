95a4a660db28d0a824296c5c843f0d404077cea1
Gopala Cheidu Raghavendrachar <ga47wif@mytum.de>
2021-04-29 23:46:13 +0200

    add basic models

---
+++ src/main/kotlin/com/krishna/model/Hospital.kt
@@ -0,0 +1,3 @@
+package com.krishna.model
+
+data class Hospital(val id: String, val name: String, val type: HospitalType, val address: String, val pin: Int)
---
+++ src/main/kotlin/com/krishna/model/HospitalType.kt
@@ -0,0 +1,8 @@
+package com.krishna.model
+
+enum class HospitalType {
+    GOVERNMENT_HOSPITAL,
+    GOVERNMENT_MEDICAL_COLLEGE,
+    PRIVATE_HOSPITAL,
+    PRIVATE_MEDICAL_COLLEGE
+}
