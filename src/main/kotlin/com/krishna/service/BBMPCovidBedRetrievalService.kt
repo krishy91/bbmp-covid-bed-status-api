a8e998e1daf6938bd107036db7a299d813f1ef12
Gopala Cheidu Raghavendrachar <ga47wif@mytum.de>
2021-05-12 23:43:09 +0200

    Add service impl

---
+++ src/main/kotlin/com/krishna/service/BBMPCovidBedRetrievalService.kt
@@ -0,0 +1,10 @@
+package com.krishna.service
+
+import com.krishna.model.HospitalBedStatus
+
+class BBMPCovidBedRetrievalService: CovidBedRetrievalService {
+
+    override fun getAllHospitalCovidBedStatus(): List<HospitalBedStatus> {
+        TODO("Not yet implemented")
+    }
+}
